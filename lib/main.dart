2c55160655f04a4b3c69e4855de8e7e2cda1694a
Lalit Rao <lalitkumaryadav9090@gmail.com>
2024-06-28 11:22:55 +0530

    with ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,92 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
-  fetchArticles();
+  runApp(MyApp());
 }
 
-Future<void> fetchArticles() async {
-  final String apiKey = 'b1fda43027ad496c8d89f8df2d316b61'; // api
-  final String url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=$apiKey';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NEWS API TEST ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-  try {
-    final response = await http.get(Uri.parse(url));
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = jsonDecode(response.body);
-      List<dynamic> articles = data['articles'];
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> _articles = [];
+  bool _isLoading = false;
+  String _error = '';
 
-      print('Fetched ${articles.length} articles:');
-      for (var article in articles) {
-        print('Title: ${article['title']}');
-        print('Description: ${article['description']}');
-        print('---');
+  Future<void> fetchArticles() async { // function for fetching the data
+    setState(() {
+      _isLoading = true;
+      _error = '';
+    });
+
+    final String apiKey = 'b1fda43027ad496c8d89f8df2d316b61'; // Replace with your API key
+    final String url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=$apiKey';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        setState(() {
+          _articles = data['articles'];
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _isLoading = false;
+          _error = 'Failed to load data: ${response.reasonPhrase}';
+        });
       }
-    } else {
-      print('Failed to load data: ${response.reasonPhrase}');
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _error = 'Failed to load data: $e';
+      });
     }
-  } catch (e) {
-    print('Failed to load data: $e');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter News API Example'),
+      ),
+      body: Center(
+        child: _isLoading
+            ? CircularProgressIndicator()
+            : _error.isNotEmpty
+            ? Text(_error)
+            : _articles.isEmpty
+            ? Text('No Data Loaded')
+            : ListView.builder(
+          itemCount: _articles.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_articles[index]['title']),
+              subtitle: Text(_articles[index]['description'] ?? 'No description available'),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchArticles,
+        tooltip: 'Fetch Articles',
+        child: Icon(Icons.download),
+      ),
+    );
   }
 }
